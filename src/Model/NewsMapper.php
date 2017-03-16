5f9cca8500a91343d7c98c55301c6000e13af305
Myroslav Tkachenko <myroslav.tkachenko@gmail.com>
2017-03-17 00:46:50 +0200

    Rewrite NewsMapper with DBAL QB

--- src/Model/NewsMapper.php
+++ src/Model/NewsMapper.php
@@ -9,10 +9,13 @@ class NewsMapper extends Mapper
 {
     public function getNews()
     {
-        $stmt = $this->db->query("SELECT * FROM news ORDER BY id DESC LIMIT 50");
+        // $stmt = $this->db->query("SELECT * FROM news ORDER BY id DESC LIMIT 50");
+        $qb = $this->db->createQueryBuilder();
+        $qb->select('*')->from('news')->orderBy('id', 'desc')->setMaxResults(50);
+        $rows = $qb->execute();
 
         $results = [];
-        while ($row = $stmt->fetch()) {
+        while ($row = $rows->fetch()) {
             $results[] = new NewsEntity($row);
         }
 
