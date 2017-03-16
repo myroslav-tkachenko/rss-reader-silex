9c6e577afb37c0b33c7b66e33607704f732a9ed8
Myroslav Tkachenko <myroslav.tkachenko@gmail.com>
2017-03-16 22:23:13 +0200

    Remove Response param from Front methods

--- src/Controller/Front.php
+++ src/Controller/Front.php
@@ -3,11 +3,13 @@
 namespace App\Controller;
 
 use \PDO;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class Front
 {
-    public function getIndex($request, $response)
+    public function getIndex(Request $request)
     {
         $db = new PDO("mysql:host=localhost;dbname=rss_news;charset=utf8", "root", "123");
         $sql = "SELECT * FROM news ORDER BY id DESC LIMIT 50";
@@ -15,11 +17,12 @@ class Front
         $stmt->execute();
         $items = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+        $response = new Response;
         $response->setContent(include '../templates/index.tpl.php');
         return $response;
     }
     
-    public function getLogin($request, $response)
+    public function getLogin(Request $request)
     {
         return $response->setContent('<form action="/login" method="POST">
             <input name="name">
@@ -28,7 +31,7 @@ class Front
         </form>');
     }
 
-    public function postLogin($request)
+    public function postLogin(Request $request)
     {
         $login = $request->request->get('name');
         $pass = $request->request->get('pass');
@@ -36,7 +39,9 @@ class Front
         $session = $request->getSession();
         if ($login == 'test' && $pass == '123') {
             $session->set('logged', true);
-            return new RedirectResponse('/cabinet');
+
+            return new Response('LOGGED');
+            // return new RedirectResponse('/cabinet');
         }
 
         return new RedirectResponse('/login');
