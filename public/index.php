1b6a7598a45d1706c0eca09308f08c308eaad257
Myroslav Tkachenko <myroslav.tkachenko@gmail.com>
2017-03-16 22:45:27 +0200

    Register SessionService

--- public/index.php
+++ public/index.php
@@ -5,9 +5,12 @@ require_once '../vendor/autoload.php';
 $app = new Silex\Application;
 $app['debug'] = true;
 
+$app->register(new Silex\Provider\SessionServiceProvider());
+
 $app->get('/', 'App\Controller\Front::getIndex');
 $app->get('/login', 'App\Controller\Front::getLogin');
 $app->post('/login', 'App\Controller\Front::postLogin');
+$app->get('/logout', 'App\Controller\Front::getLogin');
 $app->get('/cabinet', 'App\Controller\Front::getCabinet');
 
 $app->run();
