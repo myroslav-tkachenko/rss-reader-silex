d93a912870d0a8c8e8876eb45290b978f3b5488c
Myroslav Tkachenko <myroslav.tkachenko@gmail.com>
2017-03-17 01:10:04 +0200

    Add new Source form

--- templates/cabinet.tpl.php
+++ templates/cabinet.tpl.php
@@ -51,6 +51,42 @@
 
             <?php endif; ?>
 
+            <div class="panel panel-default">
+                <div class="panel-body">
+                
+                    <form action="/cabinet" method="POST" class="form-horizontal" role="form">
+                        
+                        <div class="form-group">
+                            <label for="name" class="col-sm-2 control-label">Name:</label>
+                            <div class="col-sm-10">
+                                <input type="text" name="name" id="name" class="form-control" value="" required="required" pattern="" title="">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="source_link" class="col-sm-2 control-label">Source Link:</label>
+                            <div class="col-sm-10">
+                                <input type="text" name="source_link" id="source_link" class="form-control" value="" required="required" pattern="" title="">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="rss_feed_link" class="col-sm-2 control-label">RSS Feed Link:</label>
+                            <div class="col-sm-10">
+                                <input type="text" name="rss_feed_link" id="rss_feed_link" class="form-control" value="" required="required" pattern="" title="">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-sm-10 col-sm-offset-2">
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            
+
         </div>
         
 
