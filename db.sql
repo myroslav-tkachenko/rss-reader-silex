2e9990df982300e73d15a4f272f48a755909416a
Myroslav Tkachenko <myroslav.tkachenko@gmail.com>
2017-03-17 00:53:07 +0200

    Describe SourceEntity

---
+++ db.sql
@@ -0,0 +1,32 @@
+# create database
+CREATE DATABASE rss_news CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+# create table
+CREATE TABLE `news` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `title` varchar(255) NOT NULL,
+  `link` varchar(255) NOT NULL,
+  `description` tinytext NOT NULL,
+  `source` varchar(255) NOT NULL
+) ENGINE='InnoDB';
+
+# add index
+ALTER TABLE `news` ADD UNIQUE `link` (`link`);
+
+# alter table: add column
+ALTER TABLE `news` ADD `pub_date` timestamp NOT NULL;
+
+# alter table: change column
+ALTER TABLE `news` CHANGE `pub_date` `pub_date` datetime NOT NULL AFTER `source`;
+
+# alter table: change column type
+ALTER TABLE `news`
+CHANGE `description` `description` mediumtext COLLATE 'utf8_general_ci' NOT NULL AFTER `link`; 
+
+# add sources table
+CREATE TABLE `sources` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `name` varchar(255) NOT NULL,
+  `source_link` varchar(255) NOT NULL,
+  `rss_feed_link` varchar(255) NOT NULL
+) ENGINE='InnoDB';
---
+++ src/Model/SourceEntity.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Model;
+
+class SourceEntity
+{
+    protected $id;
+    protected $name;
+    protected $source_link;
+    protected $rss_feed_link;
+
+    public function __construct(array $data)
+    {
+        // no id if we're creating
+        if (isset($data['id'])) {
+            $this->id = $data['id'];
+        }
+
+        $this->name = $data['name'];
+        $this->source_link = $data['source_link'];
+        $this->rss_feed_link = $data['rss_feed_link'];
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getSourceLink()
+    {
+        return $this->source_link;
+    }
+
+    public function getRssFeedLink()
+    {
+        return $this->rss_feed_link;
+    }
+}
