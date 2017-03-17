4c27c4e4fe191a3a864fa0bd0bc81b366bc78709
Myroslav Tkachenko <myroslav.tkachenko@gmail.com>
2017-03-17 12:40:47 +0200

    Remove Response use from Cabinet

--- src/Controller/Cabinet.php
+++ src/Controller/Cabinet.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 use App\Model\SourceMapper;
 use App\Model\SourceEntity;
